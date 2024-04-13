a3f3b28542a8cd0c9ca82914feb5e12cc823b44d
Eugene Levinson <el769@bath.ac.uk>
2024-04-13 23:46:21 +0000

    Fixed cors

--- src/server.py
+++ src/server.py
@@ -3,6 +3,8 @@ from flask import Flask
 import config as cfg
 import json
 
+from flask_cors import CORS, cross_origin
+
 
 def create_flask_app(app_cfg):
 
@@ -10,18 +12,22 @@ def create_flask_app(app_cfg):
     app.config.from_object(app_cfg)
 
     @app.route('/health')
+    @cross_origin()
     def hello():
         return status_hadnler(app)
     
     @app.route('/register_station', methods=['POST'])
+    @cross_origin()
     def register_station():
         return register_station_handler(app)
     
     @app.route('/upload-data', methods=['POST'])
+    @cross_origin()
     def upload_data():
         return upload_data_handler(app)
     
     @app.route('/upload-image', methods=['POST'])
+    @cross_origin()
     def upload_image():
         return upload_image_handler(app)
     
