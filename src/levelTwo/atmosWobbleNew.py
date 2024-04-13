6371731950bd6d34b552aa81d3c93bb7f187e7ac
peter-berthelemy <peter.berthelemy@gmail.com>
2024-04-13 17:22:01 +0100

    Changed to varience difference

--- src/levelTwo/atmosWobbleNew.py
+++ src/levelTwo/atmosWobbleNew.py
@@ -10,17 +10,24 @@ def find_brightest_star(image):
     # Find the brightest spot in the image
     #_, max_val, _, max_loc = cv2.minMaxLoc(gray)
     max_value = numpy.amax(gray)
-    print(gray.size)
+    varience = []
     #print(max_value)
     for width in range(gray.size[0]):
         for height in range(gray.size[1]):
             if gray.getpixel((width, height)) == max_value:
-                return [width,height, max_value]
+                print(numpy.arange(width-3,width+4))
+                for x in numpy.arange(width-3,width+4):
+                    for y in numpy.arange(height-3,height+4):
+                        varience.append(gray.getpixel((x, y)))
+
+                varTotal = numpy.var(varience)
+                print(varTotal)
+                return [width,height, varTotal]
 
 def track_star(images):
     # Load the first image
     first_image = Image.open(images[0]).convert('L')
-
+    varience = []
     # Find the position of the brightest star in the first image
     [width1, height1, max_value] = find_brightest_star(first_image)
     allDiffs = [0] * (len(images)-1)
@@ -29,17 +36,20 @@ def track_star(images):
         # Load the current image
         current_image = Image.open(images[i]).convert('L')
 
-        nowBrightness = current_image.getpixel((width1, height1))
+        for x in numpy.arange(width1-3,width1+4):
+            for y in numpy.arange(height1-3,height1+4):
+                varience.append(current_image.getpixel((x, y)))
 
-        diffBright = nowBrightness - max_value
+        varBright = numpy.var(varience)
+
+        diffBright = varBright - max_value
 
         # Print the distance between the positions
-        print(f"Difference in brightness: {diffBright}")
+        print(f"Difference in varience: {diffBright}")
         allDiffs[i-1] = diffBright
     
-    maxDiff = max(allDiffs)
-    ratio = maxDiff / max_value
-    return ratio
+    maxDiff = max(numpy.absolute(allDiffs))
+    return maxDiff
 
 
 # List of image file paths
