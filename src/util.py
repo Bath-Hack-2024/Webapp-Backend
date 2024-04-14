03442e399c6e71918f20faa91953b57c8fdae7c3
Eugene Levinson <el769@bath.ac.uk>
2024-04-14 02:14:58 +0000

    Added dew point

--- src/util.py
+++ src/util.py
@@ -1,12 +1,13 @@
 from data_science.now_cast import get_weather_nowcast_pressure
+from data_science.dew_point import calculate_dew_point
 
 def add_l2_data(data):
     for station in data:
         data_points = data[station]
 
         for point in data_points:
-            humidity = point.get('humidity')
-            temperature = point.get('temperature')
+            humidity = float(point.get('humidity'))
+            temperature = float(point.get('temperature'))
             barometric_pressure = float(point.get('barometric_pressure'))
             elevation = float(point.get('elevation'))
 
@@ -15,9 +16,18 @@ def add_l2_data(data):
             except Exception as e:
                 now_cast = None
 
+            try:
+                dew_point, dew_point_spread = calculate_dew_point(humidity, temperature)
+            except Exception as e:
+                dew_point = None
+                dew_point_spread = None
+
             
             
             point['now_cast'] = now_cast
+            point['dew_point'] = dew_point
+            point['dew_point_spread'] = dew_point_spread
+
 
     return data
 
